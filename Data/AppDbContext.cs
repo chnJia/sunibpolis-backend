d96ce6074579d9ae76999e4874b4035d5ca452b7
chnJia <angelangelch15@gmail.com>
2023-11-23 14:02:30 +0700

    added dbcontext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,21 @@
-﻿namespace Sunibpolis_backend.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Sunibpolis_backend.Models;
+
+namespace Sunibpolis_backend.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<CinemaLocation> CinemaLocation { get; set; }
+        public DbSet<City> City { get; set; }
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<MovieShowTime> MovieShowTime { get; set; }
+        public DbSet<PaymentMethod> PaymentMethod { get; set; }
+        public DbSet<Seat> Seat { get; set; }
+        public DbSet<Theater> Theater { get; set; }
+        public DbSet<Ticket> Ticket { get; set; }
+        public DbSet<Transaction> Transaction { get; set; }
+        public DbSet<User> User { get; set; }
     }
 }
