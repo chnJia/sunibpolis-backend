a4a6955e3efd71bd9e34655cc40ce573be4eb6f1
chnJia <angelangelch15@gmail.com>
2023-11-24 15:11:42 +0700

    updated

---
+++ Controllers/SeatController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sunibpolis_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeatController : ControllerBase
+    {
+        
+    }
+}
