80ae13f39c190f2658ebea88baf566523579f06e
chnJia <angelangelch15@gmail.com>
2023-11-24 17:27:39 +0700

    added transaction controller

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Sunibpolis_backend.Data;
+using Sunibpolis_backend.Models;
+using Sunibpolis_backend.Models.Result;
 
 namespace Sunibpolis_backend.Controllers
 {
@@ -16,5 +19,29 @@ namespace Sunibpolis_backend.Controllers
 
         // GET ALL TRANSACTION DATA
         [HttpGet]
+        public async Task<ActionResult<IEnumerable<GetTransactionResult>>> Get()
+        {
+            var transaction = await _context.Transaction
+            .OrderBy(x => x.TransactionDate)
+            .Select(x => new GetTransactionResult()
+            {
+                TransactionId = x.TransactionId,
+                TransactionStatus = x.TransactionStatus,
+                TotalPrice = x.TotalPrice,
+                TransactionDate = x.TransactionDate,
+                User = x.User,
+                Ticket = x.Ticket,
+                PaymentMethod = x.PaymentMethod
+            })
+            .ToListAsync();
+
+            var response = new ApiResponse<IEnumerable<GetTransactionResult>>
+            {
+                StatusCode = StatusCodes.Status200OK,
+                RequestMethod = HttpContext.Request.Method,
+                Data = transaction
+            };
+            return Ok(response);
+        }
     }
 }
--- Models/Result/GetTransactionResult.cs
+++ Models/Result/GetTransactionResult.cs
@@ -7,9 +7,6 @@ namespace Sunibpolis_backend.Models.Result
         public int TransactionId { get; set; }
         public string TransactionStatus { get; set; }
         public int TotalPrice { get; set; }
-        public Guid UserId { get; set; }
-        public int TicketId { get; set; }
-        public int PaymentMethodId { get; set; }
         public DateTime TransactionDate { get; set; }
 
         public User User { get; set; }
